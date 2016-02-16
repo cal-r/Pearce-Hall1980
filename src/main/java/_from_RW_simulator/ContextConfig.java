5489e7b81a16411a27fa5a739f915042a4e4f3be
rok21 <rokas.grikietis@argusmedia.com>
2016-02-16 16:48:22 +0000

    add context related stuff from R&W simulator

---
+++ src/main/java/_from_RW_simulator/ContextConfig.java
@@ -0,0 +1,158 @@
+package _from_RW_simulator;
+
+import java.io.Serializable;
+
+/**
+ * City University
+ * BSc Computing with Artificial Intelligence
+ * Project title: Building a TD Simulator for Real-Time Classical Conditioning
+ * @supervisor Dr. Eduardo Alonso
+ * @author Jonathan Gray
+ */
+public class ContextConfig implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 7555945696185751652L;
+
+    /**
+     * Enum holding context symbols.
+     * City University
+     * BSc Computing with Artificial Intelligence
+     * Project title: Building a TD Simulator for Real-Time Classical Conditioning
+     * @supervisor Dr. Eduardo Alonso
+     * @author Jonathan Gray
+     *
+     */
+
+    public enum Context implements Serializable {
+        EMPTY(""),PHI("\u03A6"), PSI("\u03A8"), THETA("\u0398"), XI("\u039E"),
+        // AF July-2012
+        OMEGA("\u03A9"), KAPPA("\u039A");
+        private Context(String name) {
+            this.name = name;
+        }
+        /** Symbol. **/
+        private String name;
+
+        public String toString() {
+            return name;
+        }
+
+        /**
+         * Check if a cue is a context.
+         * @param in
+         * @return
+         */
+
+        public static boolean isContext(final String in) {
+            for(Context ctxt : Context.values()) {
+                if(ctxt.name.equals(in)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         *
+         * @return an array of all the contexts except the empty one.
+         */
+
+        public static Context[] getList() {
+            Context[] list = new Context[] {PHI, PSI, THETA, XI, OMEGA, KAPPA};
+            return list;
+        }
+
+        // Alberto Fernandez. August-2012
+
+        /**
+         * Check if a string contains a context.
+         * @param in
+         * @return
+         */
+
+        public static boolean containsContext(final String in) {
+            for(Context ctxt : Context.getList()) {
+                if(in.contains(ctxt.name)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+
+    /** Symbol this context is a config for. **/
+    private Context context;
+    /** Alpha for this context. **/
+    private double alpha;
+    /** Empty context. **/
+    public static final ContextConfig EMPTY = new ContextConfig(Context.EMPTY, 0);
+    /** Default alpha. **/
+    private static double defaultAlpha = 0.15;
+    /** Default context. **/
+    private static Context defaultContext = Context.PHI;
+    /** Whether default has been updated. **/
+    private static boolean defaultAlphaUpdated = false;
+    private static boolean defaultContextUpdated = false;
+
+    public ContextConfig() {
+        context = defaultContext;
+        alpha = defaultAlpha;
+    }
+
+    public ContextConfig(final Context context, final double alpha) {
+        this.context = context;
+        this.alpha = alpha;
+    }
+
+    /**
+     * @return the context
+     */
+    public Context getContext() {
+        return context;
+    }
+    /**
+     * @param context the context to set
+     */
+    public void setContext(Context context) {
+        this.context = context;
+        if(!defaultContextUpdated) {
+            defaultContext = context;
+            defaultContextUpdated = true;
+        }
+    }
+    /**
+     * @return the alpha value
+     */
+    public Double getAlpha() {
+        return alpha;
+    }
+    /**
+     * @param alpha the alpha value to set
+     */
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+        if(!defaultAlphaUpdated) {
+            defaultAlpha = alpha;
+            defaultAlphaUpdated = true;
+        }
+    }
+
+    public String getSymbol() {
+        return context.toString();
+    }
+
+    public String toString() {
+        return context + "("+getAlpha().floatValue()+")";
+    }
+
+    public static void clearDefault() {
+        defaultAlpha = 0.15;
+        defaultAlphaUpdated = false;
+        defaultContext = Context.PHI;
+        defaultContextUpdated = false;
+    }
+}
