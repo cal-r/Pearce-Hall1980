83e4a8824b01dc457bbdd7719c480cddc5e42a70
rok21 <rokas.grikietis@argusmedia.com>
2016-03-16 17:00:49 +0000

    different US per phase: consistency for lines with negative labelling

--- src/main/java/Helpers/Graphing/GraphStringsHelper.java
+++ src/main/java/Helpers/Graphing/GraphStringsHelper.java
@@ -13,7 +13,15 @@ public class GraphStringsHelper {
     }
 
     public static String getLineCommand(GraphLine line){
-        return String.format("%s%s%s", line.getGroup().getName(), ActionCommands.GROUP_LINE_SEPARATOR, line.getName());
+        String lineName = getNameWithoutNegativeLabeling(line.getName());
+        return String.format("%s%s%s", line.getGroup().getName(), ActionCommands.GROUP_LINE_SEPARATOR, lineName);
+    }
+
+    private static String getNameWithoutNegativeLabeling(String name){
+        if (name.endsWith(")-")){
+            return name.substring(1, name.length()-2);
+        }
+        return name;
     }
 
     public static String getGroupCommand(GraphLineGroup group){
